2ead2a318b3d914c631de52548a5212a26fbd9e7
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-03-15 00:05:45 +0100

    add error message when trying to book past competition

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from flask import Flask,render_template,request,redirect,flash,url_for
 
 
@@ -40,8 +41,12 @@ def create_app(config):
     def book(competition,club):
         foundClub = [c for c in clubs if c['name'] == club][0]
         foundCompetition = [c for c in competitions if c['name'] == competition][0]
+
+        now = datetime.now()
+        date_time_now = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(foundCompetition)
         if foundClub and foundCompetition:
-            return render_template('booking.html',club=foundClub,competition=foundCompetition)
+            return render_template('booking.html',club=foundClub,competition=foundCompetition, date=date_time_now)
         else:
             flash("Something went wrong-please try again")
             return render_template('welcome.html', club=club, competitions=competitions)
@@ -50,6 +55,7 @@ def create_app(config):
     @app.route('/purchasePlaces',methods=['POST'])
     def purchasePlaces():
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+        print(competitions)
         club = [c for c in clubs if c['name'] == request.form['club']][0]
         placesRequired = int(request.form['places'])
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
